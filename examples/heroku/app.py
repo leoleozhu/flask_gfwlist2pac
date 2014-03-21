c6e0d5ecce4ea505d36efae38b9c709289e3dcab
Leo Zhu <Leiping.Zhu@gmail.com>
2014-03-21 10:11:12 +0800

    initial commit

---
+++ examples/heroku/app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask.ext.gfwlist2pac import Pac
+
+import os
+
+app = Flask(__name__)
+
+pac = Pac(app, url='/')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
